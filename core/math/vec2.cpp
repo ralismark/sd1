e074b7c27526fbeaa632e83080e5d676200288c7
endermaster139 <timmy.yao@outlook.com.au>
2016-06-07 23:30:14 +1000

    Improve vec2 class
     - Change vec2::dist2() to vec2::dist(), now gets actual distance, not
       squared
     - Add operators to multiply and divide with double, including with
       assignment
     - Add vec2::dot() (dot product), vec2::norm() (normalised) and
       vec2::abs() (absolute)

--- core/math/vec2.cpp
+++ core/math/vec2.cpp
@@ -1,5 +1,7 @@
 #include "vec2.hpp"
 
+#include <cmath>
+
 namespace math {
 
 vec2::vec2()
@@ -34,7 +36,27 @@ vec2 vec2::operator-(const vec2& other)
 
 vec2 vec2::operator*(vec2 other)
 {
-	return other -= *this;
+	return other *= *this;
+}
+
+vec2 vec2::operator*(double scale)
+{
+	return vec2(x * scale, y * scale);
+}
+
+vec2 vec2::operator/(double scale)
+{
+	return vec2(x / scale, y / scale);
+}
+
+vec2 operator*(double scale, vec2 vec)
+{
+	return vec * scale;
+}
+
+vec2 operator/(double scale, vec2 vec)
+{
+	return vec / scale;
 }
 
 vec2& vec2::operator+=(const vec2& other)
@@ -58,9 +80,39 @@ vec2& vec2::operator*=(const vec2& other)
 	return *this;
 }
 
-double vec2::dist2()
+vec2& vec2::operator/=(double scale)
 {
-	return x * y;
+	x *= scale;
+	y *= scale;
+	return *this;
+}
+
+vec2& vec2::operator/=(double scale)
+{
+	x /= scale;
+	y /= scale;
+	return *this;
+}
+
+double vec2::dist()
+{
+	return std::sqrt(x * y);
+}
+
+double vec2::dot(const vec2& other)
+{
+	return x * other.x + y * other.y;
+}
+
+vec2 vec2::norm()
+{
+	double dist = dist();
+	return vec2(x / dist, y / dist);
+}
+
+vec2 vec2::abs()
+{
+	return vec2(std::abs(x), std::abs(y));
 }
 
 }
--- core/math/vec2.hpp
+++ core/math/vec2.hpp
@@ -17,11 +17,22 @@ public:
 	vec2 operator-(vec2 other) const;
 	vec2 operator*(vec2 other) const;
 
+	vec2 operator*(double scale) const;
+	vec2 operator/(double scale) const;
+	friend vec2 operator*(double scale, vec2 vec);
+	friend vec2 operator/(double scale, vec2 vec);
+
 	vec2& operator+=(const vec2& other);
 	vec2& operator-=(const vec2& other);
 	vec2& operator*=(const vec2& other);
 
-	double dist2() const;
-}
+	vec2& operator*=(double scale);
+	vec2& operator/=(double scale);
+
+	double dist() const;
+	double dot(const vec2& other) const;
+	vec2 norm() const;
+	vec2 abs() const;
+};
 
 }
