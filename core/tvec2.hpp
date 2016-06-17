40dc62a77e684883634e7c7dd8c839ce17ef68ac
endermaster139 <timmy.yao@outlook.com.au>
2016-06-17 23:08:56 +1000

    Finally add tvec2
    Very dodgy but whatever, just have it here for when I might use it. Now
    I can finally work on the other important bits.

---
+++ core/tvec2.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "../expt/proxy.hpp"
+#include "vec2.hpp"
+
+class tvec2
+{
+public:
+	enum struct side { x, y };
+	static double get(side s, void* self, double v);
+	static double set(side s, void* self, double v);
+
+	vec2* parent;
+	vec2 off;
+	proxy<double> x, y;
+public:
+	tvec2(double v = 0);
+	tvec2(double x_init, double y_init);
+
+	tvec2(vec2* p, double v = 0);
+	tvec2(vec2* p, double x_init, double y_init);
+};
