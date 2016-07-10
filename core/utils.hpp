1bdd0027f07689895798652290f233faf7de8044
endermaster139 <timmy.yao@outlook.com.au>
2016-07-10 20:38:43 +1000

    Add bind_instance to utils.hpp
    Based off bind_first, binds an object to a member function, so it can be
    called like a normal function.

--- core/utils.hpp
+++ core/utils.hpp
@@ -20,13 +20,28 @@ namespace std {
 }
 
 template <typename R, typename T, typename...Types, typename U, int... indices>
-std::function<R(Types...)> bind_first(std::function<R(T,Types...)> f, U val, std::integer_sequence<int, indices...> /*seq*/) {
+std::function<R(Types...)> bind_first(std::function<R(T, Types...)> f, U val, std::integer_sequence<int, indices...> /*seq*/) {
 	return std::bind(f, val, placeholder<indices + 1>::ph...);
 }
 
 template <typename R, typename T, typename...Types, typename U>
 std::function<R(Types...)> bind_first(std::function<R(T, Types...)> f, U val) {
 	return bind_first(f, val, std::make_integer_sequence<int, sizeof...(Types)>());
+}
+
+// Based off above stuff
+// Binds instance of a class to a class method
+
+template <typename C, typename R, typename... T, int... indices>
+std::function<R(T...)> bind_instance(R (C::*f)(T...), C& obj, std::integer_sequence<int, indices...> /*seq*/)
+{
+	return std::bind(f, obj, placeholder<indices + 1>::ph...);
+}
+
+template <typename C, typename R, typename... T>
+std::function<R(T...)> bind_instance(R (C::*f)(T...), C& obj)
+{
+	return bind_instance(f, obj, std::make_integer_sequence<int, sizeof...(T)>{});
 }
 
 #endif // BIND_FIRST
