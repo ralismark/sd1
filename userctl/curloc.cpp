0f4046d008eaf1e5f718a65585836c8bc31552c7
endermaster139 <timmy.yao@outlook.com.au>
2016-08-15 23:03:52 +1000

    (userctl) Add bound vec for cursor position

--- userctl/curloc.cpp
+++ userctl/curloc.cpp
@@ -3,13 +3,29 @@
 #include <sfml/window/mouse.hpp>
 #include "disp/window.hpp" // Assuming stdwin
 
+namespace
+{
+
+	int get_x()
+	{
+		return sf::Mouse::getPosition(disp::stdwin).x;
+	}
+
+	int get_y()
+	{
+		return sf::Mouse::getPosition(disp::stdwin).y;
+	}
+
+}
+
 namespace userctl
 {
 
 	vec2i cursor_loc()
 	{
-		auto pos = sf::Mouse::getPosition(disp::stdwin);
-		return make_vec<int>(pos.x, pos.y);
+		return make_vec<int>(get_x(), get_y());
 	}
+
+	bvec<int, 2> cursor_vec = {get_x, get_y};
 
 }
--- userctl/curloc.hpp
+++ userctl/curloc.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #include "math/vec.hpp"
+#include "math/bvec.hpp"
 
 namespace userctl
 {
 
 	vec2i cursor_loc();
+	extern bvec<int, 2> cursor_vec;
 
 }
