5377bee1fc55af903564c528ae25749c92bd304c
endermaster139 <timmy.yao@outlook.com.au>
2016-08-15 23:21:48 +1000

    Add rectangle drawing function
    One draws border, one draws it filled

--- disp/geometry.cpp
+++ disp/geometry.cpp
@@ -4,6 +4,7 @@
 
 #include <cmath>
 #include <sfml/graphics/convexshape.hpp>
+#include <sfml/graphics/rectangleshape.hpp>
 
 namespace disp
 {
@@ -31,6 +32,32 @@ namespace disp
 		}
 
 		stdwin->draw(vertices, 4, sf::Quads);
+	}
+
+	void fill_rect(rect<double> r, int co)
+	{
+		sf::RectangleShape rs;
+		rs.setSize(sf::Vector2f(r->w, r->h));
+		rs.setPosition(r->x1, r->x2);
+
+		rs.setOutlineColor(sf::Color::Transparent);
+		rs.setFillColor(as_color(co));
+
+		stdwin->draw(rs);
+	}
+
+	void border_rect(rect<double> r, int co, size_t width)
+	{
+		sf::RectangleShape rs;
+		rs.setSize(sf::Vector2f(r->w, r->h));
+		rs.setPosition(r->x1, r->x2);
+
+		rs.setFillColor(sf::Color::Transparent);
+		rs.setOutlineColor(as_color(co));
+
+		rs.setOutlineThickness(width);
+
+		stdwin->draw(rs);
 	}
 
 }
--- disp/geometry.hpp
+++ disp/geometry.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
 #include "math/vec.hpp"
+#include "math/rect.hpp"
 
 namespace disp
 {
 
 	void line(vec2 begin, vec2 end, int co = 0xffffff, size_t width = 1);
+
+	void fill_rect(rect<double> r, int co = 0xffffff);
+	void border_rect(rect<double> r, int co = 0xffffff, size_t width = 1);
 
 }
