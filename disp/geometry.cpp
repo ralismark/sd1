6e453e5d7b6fb933d3a3d8f59e53140b36acecf7
endermaster139 <timmy.yao@outlook.com.au>
2016-08-15 23:21:03 +1000

    Add line drawing function
    You can have arbitrary thickness and colour (inc. transparency)

---
+++ disp/geometry.cpp
@@ -0,0 +1,36 @@
+#include "geometry.hpp"
+
+#include "window.hpp"
+
+#include <cmath>
+#include <sfml/graphics/convexshape.hpp>
+
+namespace disp
+{
+
+	void line(vec2 begin, vec2 end, int co, size_t width)
+	{
+		vec2 dir = end - begin;
+		vec2 unit_dir = dir / std::sqrt(dir->x * dir->x + dir->y * dir->y);
+		vec2 unit_perp= { -unit_dir->y, unit_dir->x };
+
+		vec2 offset = (width / 2.0) * unit_perp;
+
+		gvec<double, 2> vert[4];
+		vert[0] = begin + offset;
+		vert[1] = end + offset;
+		vert[2] = end - offset;
+		vert[3] = begin - offset;
+
+		sf::Vertex vertices[4];
+
+		sf::Color c = as_color(co);
+		for(int i = 0; i < 4; ++i) {
+			vertices[i].color = c;
+			vertices[i].position = {vert[i]->x, vert[i]->y};
+		}
+
+		stdwin->draw(vertices, 4, sf::Quads);
+	}
+
+}
---
+++ disp/geometry.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "math/vec.hpp"
+
+namespace disp
+{
+
+	void line(vec2 begin, vec2 end, int co = 0xffffff, size_t width = 1);
+
+}
