1b744b0676c984963df180028d98636013a22365
endermaster139 <timmy.yao@outlook.com.au>
2016-08-21 00:25:37 +1000

    Make build tools runnable from anywhere
    Literally anywhere, even out of tree

--- build.bat
+++ build.bat
@@ -26,13 +26,13 @@ goto :eof
 	type %tmpfile%1 | findstr /L /C:"<<" | sed "s/.*<<\(.*\)>>.*/\1/g" > %tmpfile%2
 
 	echo:Build done:
-	
+
 	set cnt=0
 	for /F %%f in (%tmpfile%2) do (
 		set /A cnt += 1
 		echo: !cnt!^) output %%f
-		for /F "usebackq" %%d in (`stat -c "%%Y" %%f`) do (
-			sqlite3 .tup\db "update node set mtime=%%d where type=4 and name='%%~nxf'"
+		for /F "usebackq" %%d in (`stat -c "%%Y" "%~dp0%%f"`) do (
+			sqlite3 "%~dp0.tup\db" "update node set mtime=%%d where type=4 and name='%%~nxf'"
 		)
 	)
 
@@ -59,7 +59,7 @@ goto :eof
 :force
 
 	echo:Force rebuilding
-	for /R %%i in (core\* debug\* disp\* math\* userctl\*) do @touch "%%i"
+	for /R %%i in ("%~dp0*.?pp") do @touch "%%i"
 	echo:
 
 	goto :build
--- setup.bat
+++ setup.bat
@@ -2,7 +2,7 @@
 
 if "%done-setup%" == "" (
 	set "done-setup=1"
-	set "path=%path%;%~dp0\bin"
+	set "path=%path%;%~dp0\bin;%~dp0"
 	echo Setup done
 ) else (
 	echo Already done setup
